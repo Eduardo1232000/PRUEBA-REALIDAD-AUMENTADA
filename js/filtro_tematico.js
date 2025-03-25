a8d26750baffb4390df9a5ac7a820d4b068fb4d3
Eduardo Reyes <eduardoalex2000@hotmail.com>
2025-03-25 16:16:18 -0600

    validacion

--- js/filtro_tematico.js
+++ js/filtro_tematico.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const boton_cambiar_camara = document.querySelector("#boton_cambiar_camara");
     //sessionStorage.setItem("textura_personalizada","./img/shrek.png" )
     let imagen_tematica = sessionStorage.getItem("textura_personalizada")
-    if(imagen_tematica === "" || imagen_tematica === undefined){
+    if(imagen_tematica === "" || imagen_tematica === undefined || imagen_tematica===null){
         imagen_tematica = "./img/viejito.png"
     }
     console.log(imagen_tematica)
